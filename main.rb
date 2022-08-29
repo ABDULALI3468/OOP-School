efa4c5ba7ee69a62a724dffb758155d465565837
Sanja <sanja1mandic@gmail.com>
2022-08-29 14:41:09 +0200

    - Devide funnction menu to the options and act_regarding_input functions - In options make switch for all user choice, when each method need to be called - In act_regarding input do looping regarding which choice was made

--- main.rb
+++ main.rb
@@ -10,38 +10,47 @@ class Main
 
   include InputModule
 
-  def menu_options
+  def display_options
     "
-M E N U
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit
-Please choose an option by entering a number between 1 and 7: "
+      M E N U
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit
+      Please choose an option by entering a number between 1 and 7: "
   end
 
-  def menu
+  def options(input)
+    case input
+    when 1
+      app.list_books
+    when 2
+      app.print_person
+    when 3
+      app.add_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.list_rentals
+    else
+      puts 'You put the wrong input. Please enter a number between 1 and 7.'
+    end
+  end
+
+  def act_regarding_input
     loop do
-      input = numeric_input(menu_options, (1..7))
-      case input
-      when 1
-        app.list_books
-      when 2
-        app.print_person
-      when 3
-        app.add_person
-      when 4
-        app.create_book
-      when 5
-        app.create_rental
-      when 6
-        app.list_rentals
-      else
-        exit
+      display_options
+      choice = gets.to_i
+      if choice == 7
+        puts 'Thank You for using my School Library!'
+        break
       end
+      options(choice)
     end
   end
 
