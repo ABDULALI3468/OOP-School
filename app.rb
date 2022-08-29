201b1aca2b5d8d99d029fe856f1a3383f253ebd7
Sanja <sanja1mandic@gmail.com>
2022-08-29 15:13:53 +0200

    - Pase all function from Main class - Change print_person to have option when list is empty and make it more dry -  Change list_books to have option when list is empty and make it more dry - Change method create_rental to put number before given list on options and make it more dry

--- app.rb
+++ app.rb
@@ -16,6 +16,50 @@ class App
 
   include InputModule
 
+  def display_options
+    puts "
+      M E N U
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit
+      Please choose an option by entering a number between 1 and 7: "
+  end
+
+  def options(input)
+    case input
+    when 1
+      list_books
+    when 2
+      print_person
+    when 3
+      add_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    else
+      puts 'You put the wrong input. Please enter a number between 1 and 7.'
+    end
+  end
+
+  def act_regarding_input
+    loop do
+      display_options
+      choice = gets.to_i
+      if choice == 7
+        puts 'Thank You for using my School Library!'
+        break
+      end
+      options(choice)
+    end
+  end
+
   def add_person
     text = 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     type = numeric_input(text, (1..2))
@@ -42,13 +86,10 @@ class App
   end
 
   def print_person
-    return 'No people to list' if @persons.empty?
-
-    result = ''
-    @persons.each_with_index do |person, index|
-      result += "#{index}) [#{person.class.name}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}\n"
+    puts 'People list is empty! Add a person.' if @persons.empty?
+    @persons.map do |person|
+      p "#{person.class.name} Name: #{person.name}, Age: #{person.age}, id: #{person.id}"
     end
-    print result
   end
 
   def create_book
@@ -62,28 +103,26 @@ class App
   end
 
   def list_books
-    i = 0
-    while i < @books.length
-      puts "#{i}) Title: #{@books[i].title}, Author: #{@books[i].author}"
-      i += 1
+    puts 'Book list is empty! Add a book.' if @books.empty?
+    @books.map do |book|
+      puts %(Title: "#{book.title}", Author: #{book.author})
     end
   end
 
   def create_rental
-    select_books = 'Select a book from the following list by number '
-    puts select_books
-    list_books
+    puts 'Select a book from the following list by number:'
+    @books.each_with_index { |book, index| puts %(#{index}\) Title: "#{book.title}", Author: #{book.author}) }
     book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not id)'
-    print_person
+
+    @persons.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
     person_index = gets.chomp.to_i
+
     print 'Date: '
-
     date = gets.chomp.to_i
-    person = @persons[person_index]
-    book = @books[book_index]
 
-    @rentals << Rental.new(date, book, person)
+    @rentals << Rental.new(date, @books[book_index], @persons[person_index])
     puts 'Rental created sucessfully'
   end
 
